df5abeaed2a28a9f473807dece09b85bba8b5e07
Schneyer <I23764@verisk.com>
2020-10-19 12:54:21 -0400

    new png commit

--- plot1.R
+++ plot1.R
@@ -8,13 +8,12 @@ SCC <- readRDS("Source_Classification_Code.rds")
 sumPollutant <- aggregate(NEI$Emissions, by = list(year=NEI$year), FUN = sum)
 
 options(scipen = 5)
-plot(sumPollutant$year, sumPollutant$x, type = c("line"),
+plot(sumPollutant$year, sumPollutant$x, type = c("l"),
      main = expression('Total PM '[2.5]*' Over Time'),
      xlab = "Year",
      ylab = expression('Total PM '[2.5]*' concentration (tons)'))
 
-png(filename = "Plot1.png")
+dev.copy(png,'plot1.png')
 dev.off()
-
 
 
